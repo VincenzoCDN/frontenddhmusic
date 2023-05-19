74a428e0f50b9ff98ce78e94d3b954a1d6527abb
Matteo Strazzera <99515747+Matstraz@users.noreply.github.com>
2023-05-19 20:58:12 +0200

    feat: add main graph + register button

--- src/components/Music.js
+++ src/components/Music.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import logo3 from "../assets/logo3.png";
-import logo1 from "../assets/logo1.png";
+import SignUpModal from "./SignUpModal";
 
 const App = () => {
   const [email, setemail] = useState("");
   const [password, setPassword] = useState("");
-  const [isRegistered, setIsRegistered] = useState(false);
+  /*   const [isRegistered, setIsRegistered] = useState(false); */
 
   const navigate = useNavigate();
 
@@ -18,7 +18,7 @@ const App = () => {
     setPassword(event.target.value);
   };
 
-  function handleFormSubmit /* = async (event) => */(event) {
+  function handleLogin(event) {
     event.preventDefault();
     let myStatus = "";
 
@@ -47,7 +47,7 @@ const App = () => {
           myData !== "Invalid password"
         ) {
           alert("User logged in successfully");
-          setIsRegistered(true);
+          navigate("/music1");
         } else {
           alert(myStatus);
         }
@@ -59,49 +59,66 @@ const App = () => {
     <div className="h-screen w-screen bg-red-800">
       <nav className="flex justify-between items-center border-b-2 border-white bg-blue-900 mainContainer">
         <img src={logo3} alt="logo 3" className="px-2 py-1 w-48" />
-        <form
-          className="flex justify-end items-center gap-2 py-1"
-          onSubmit={handleFormSubmit}
-        >
-          <input
-            type="text"
-            value={email}
-            onChange={handleemailChange}
-            placeholder="E-mail"
-            className="border-2 border-blue-500 px-3 py-0 bg-slate-300"
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={handlePasswordChange}
-            className="border-2 border-blue-500 px-3 py-0 bg-slate-300"
-          />
-          {!isRegistered && (
+        <div className="flex justify-end items-center pr-2">
+          <form
+            className="flex justify-center items-center gap-2 py-1 pr-0"
+            onSubmit={handleLogin}
+          >
+            <input
+              type="text"
+              value={email}
+              onChange={handleemailChange}
+              placeholder="E-mail"
+              className="border-2 border-blue-500 px-3 py-0 bg-slate-300"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={handlePasswordChange}
+              className="border-2 border-blue-500 px-3 py-0 bg-slate-300"
+            />
             <button
               type="submit"
               className="border-2 border-blue-500 m-0 px-3 py-0 bg-slate-300"
             >
               Login
             </button>
-          )}
-        </form>
+          </form>
+          <SignUpModal />
+        </div>
       </nav>
-      <div className="secondContainer bg-gradient-to-b from-blue-900 to-blue-400 flex justify-center items-center">
-        {!isRegistered && <p>Welcome to SITE TITLE</p>}
-        {isRegistered && (
-          <div className="relative">
-            <img
-              src={logo1}
-              alt="logo 1"
-              className="cursor-pointer"
-              onClick={() => navigate("/music1")}
-            />
-            <p className="absolute left-1/2 bottom-1/4 -translate-x-1/2">
-              Go to your dashboard!
-            </p>
+      <div className="secondContainer bg-gradient-to-b from-blue-900 to-blue-400 flex justify-center items-center gap-5">
+        <div className="w-1/4 h-full flex justify-center items-center">
+          <div className="flex flex-col justify-center items-center h-2/3 w-full border-violet-900 border-2">
+            <div className="h-2/3 w-full border-b-white border-b bgMain1"></div>
+            <div className="flex justify-center items-center h-1/3 w-full">
+              <div className="w-1/3 bgMainA1 h-full"></div>
+              <div className="w-1/3 bgMainA2 h-full border-8 border-yellow-100"></div>
+              <div className="w-1/3 bgMainA3 h-full"></div>
+            </div>
           </div>
-        )}
+        </div>
+        <div className="w-1/4 h-full flex justify-center items-center">
+          <div className="flex flex-col justify-center items-center h-2/3 w-full border-violet-900 border-2">
+            <div className="h-2/3 w-full border-b-white border-b bgMain2"></div>
+            <div className="flex justify-center items-center h-1/3 w-full">
+              <div className="w-1/3 bgMainB1 h-full"></div>
+              <div className="w-1/3 bgMainB2 h-full border-8 border-violet-300"></div>
+              <div className="w-1/3 bgMainB3 h-full"></div>
+            </div>
+          </div>
+        </div>
+        <div className="w-1/4 h-full flex justify-center items-center">
+          <div className="flex flex-col justify-center items-center h-2/3 w-full border-violet-900 border-2">
+            <div className="h-2/3 w-full border-b-white border-b bgMain3"></div>
+            <div className="flex justify-center items-center h-1/3 w-full">
+              <div className="w-1/3 bgMainC1 h-full"></div>
+              <div className="w-1/3 bgMainC2 h-full border-8 border-orange-300"></div>
+              <div className="w-1/3 bgMainC3 h-full"></div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/SignUpModal.js
@@ -0,0 +1,226 @@
+import React, { useState } from "react";
+
+export default function SignUpModal() {
+  const [showModal, setShowModal] = React.useState(false);
+
+  const [allUserData, setAllUserData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    name: "",
+    surname: "",
+    dateOfBirth: "",
+    gender: "",
+    nationality: "",
+  });
+
+  const handleInput = (event) => {
+    const { name, type, value, checked } = event.target;
+    setAllUserData((data) => {
+      return {
+        ...data,
+        [name]: type !== "checkbox" ? value : checked,
+      };
+    });
+  };
+
+  function register(e) {
+    e.preventDefault();
+
+    if (
+      localStorage.getItem("currentUser") &&
+      localStorage.getItem("currentUser").includes(allUserData.email)
+    ) {
+      alert("Email already exists");
+    } else {
+      localStorage.setItem(
+        "currentUser",
+        allUserData.email + allUserData.password
+      );
+
+      fetch("http://localhost:8080/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(allUserData),
+      })
+        .then((res) => res.text())
+        .then((data) => console.log(data));
+    }
+
+    console.log(allUserData);
+  }
+
+  return (
+    <>
+      <button
+        className="border-2 border-blue-500 m-0 px-3 py-0 bg-slate-300"
+        onClick={() => {
+          setShowModal(true);
+        }}
+      >
+        Register
+      </button>
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-gradient-to-t from-myBlue-700 to-myBlue-300 text-white outline-none focus:outline-none bg-blue-900">
+                <form className="w-72 sm:w-96" onSubmit={register}>
+                  <h1 className="text-xl text-center my-6 ">
+                    Sign up and start listening to Music!
+                  </h1>
+                  <div className="mb-1 px-4 sm:mb-2">
+                    <input
+                      placeholder="Username"
+                      required
+                      type="text"
+                      className=" text-black flex-grow w-full py-1 px-4 mb-2 transition duration-200 border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                      name="username"
+                      onChange={handleInput}
+                      value={allUserData.username}
+                    />
+                  </div>
+                  <div className="mb-1 px-4 sm:mb-2">
+                    <input
+                      placeholder="E-mail"
+                      required
+                      type="email"
+                      className=" text-black flex-grow w-full py-1 px-4 mb-2 transition duration-200 border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                      name="email"
+                      onChange={handleInput}
+                      value={allUserData.email}
+                    />
+                  </div>
+                  <div className="mb-1 px-4 sm:mb-2">
+                    <input
+                      placeholder="Password"
+                      required
+                      type="password"
+                      className=" text-black flex-grow w-full py-1 px-4 mb-2 transition duration-200 border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                      id="password"
+                      name="password"
+                      onChange={handleInput}
+                      value={allUserData.password}
+                    />
+                  </div>
+                  <div className="mb-1 px-4 sm:mb-2">
+                    <input
+                      placeholder="First Name"
+                      required
+                      type="text"
+                      className=" text-black flex-grow w-full py-1 px-4 mb-2 transition duration-200 border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                      name="name"
+                      onChange={handleInput}
+                      value={allUserData.name}
+                    />
+                  </div>
+                  <div className="mb-1 px-4 sm:mb-2">
+                    <input
+                      placeholder="Last Name"
+                      required
+                      type="text"
+                      className=" text-black flex-grow w-full py-1 px-4 mb-2 transition duration-200 border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                      name="surname"
+                      onChange={handleInput}
+                      value={allUserData.surname}
+                    />
+                  </div>
+                  <div className="mb-1 px-4 sm:mb-2">
+                    <label>Date Of Birth</label>
+                    <input
+                      required
+                      type="date"
+                      className=" text-black flex-grow w-full py-1 px-4 mb-2 transition duration-200 border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                      name="dateOfBirth"
+                      onChange={handleInput}
+                      value={allUserData.dateOfBirth}
+                      min="1900-01-01"
+                      max="2099-12-31"
+                    />
+                  </div>
+
+                  <fieldset className="flex justify-center items-center border-2 border-black m-2">
+                    <legend>Gender</legend>
+                    <div className="flex justify-center items-center w-1/4">
+                      <label>Male</label>
+                      <div className="mb-1 px-4 sm:mb-2">
+                        <input
+                          placeholder="Gender"
+                          required
+                          type="radio"
+                          className=""
+                          name="gender"
+                          onChange={handleInput}
+                          value={"male"}
+                        />
+                      </div>
+                    </div>
+
+                    <div className="flex justify-center items-center w-1/4">
+                      <label>Female</label>
+
+                      <div className="mb-1 px-4 sm:mb-2">
+                        <input
+                          placeholder="Gender"
+                          required
+                          type="radio"
+                          className=""
+                          name="gender"
+                          onChange={handleInput}
+                          value={"female"}
+                        />
+                      </div>
+                    </div>
+                  </fieldset>
+                  <div className="mb-1 px-4 sm:mb-2">
+                    {" "}
+                    <label>Nationality</label>
+                    <select
+                      className=" text-black flex-grow w-full py-1 px-4 mb-2 transition duration-200 border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                      onChange={handleInput}
+                      value={allUserData.nationality}
+                      name="nationality"
+                    >
+                      <option></option>
+                      <option>English</option>
+                      <option>Italian</option>
+                      <option>Chinese</option>
+                      <option>French</option>
+                      <option>German</option>
+                      <option>Japanese</option>
+                      <option>Portuguese</option>
+                      <option>Spanish</option>
+                    </select>
+                  </div>
+                  <div className="mt-4 px-1 mb-2 sm:mb-4 flex justify-between">
+                    <button
+                      className="border-myBlue-100 bg-myBlue-700 border-2  rounded-md items-center justify-center px-6 font-medium hover:text-slate-400 text-slate-200 mx-3 mb-5 py-1"
+                      type="button"
+                      onClick={() => setShowModal(false)}
+                    >
+                      Close
+                    </button>
+                    <button
+                      type="submit"
+                      className="border-myBlue-100 bg-myBlue-700 border-2  rounded-md items-center justify-center hover:text-slate-400 font-medium text-slate-200 mx-3 mb-5 w-full px-6 py-1"
+                    >
+                      Register
+                    </button>
+                  </div>
+                </form>
+
+                {/*body*/}
+                {/*footer*/}
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  );
+}
