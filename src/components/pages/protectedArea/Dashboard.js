2b912af55727be3ed183b26754869268f788b004
Matteo Strazzera <99515747+Matstraz@users.noreply.github.com>
2023-05-20 20:30:21 +0200

    feat : add dashboard artist fetch

--- src/components/pages/protectedArea/Dashboard.js
+++ src/components/pages/protectedArea/Dashboard.js
@@ -1,73 +1,69 @@
 import { useEffect, useState } from "react";
-import logo1 from "../../../assets/logo1.png";
+/* import logo1 from "../../../assets/logo1.png"; */
 import Navbar from "../../common/Navbar";
 import Footer from "../../common/Footer";
 
 export default function Dashboard() {
-  const [video, setVideo] = useState();
+  /* const [video, setVideo] = useState();
   const [title, setTitle] = useState();
+  const [artistName, setArtistName] = useState();
+  const [song, setSong] = useState(0); */
   const [role, setRole] = useState();
   const [id, setId] = useState();
   const [username, setUsername] = useState();
-  const [artistName, setArtistName] = useState();
-  const [song, setSong] = useState(0);
+  const [artistData, setArtistData] = useState([]);
 
+  //GET ID
 
-//GET ID
+  useEffect(() => {
+    let myToken = localStorage.getItem("My Token");
+    let myCompleteToken = "Bearer " + myToken;
+    fetch("http://localhost:8080/front/get-id", {
+      method: "GET",
+      headers: {
+        Authorization: myCompleteToken,
+      },
+    })
+      .then((data) => data.text())
+      .then((data) => setId(data));
+  }, []);
 
-useEffect(() => {
-  let myToken = localStorage.getItem("My Token");
-  let myCompleteToken = "Bearer "+ myToken
-  fetch('http://localhost:8080/front/get-id', {
-    method: "GET",
-    headers: {
-   'Authorization': myCompleteToken,
- },
-})
-.then((data) => data.text())
-.then((data) => setId(data))
-}, []); 
+  //GET USERNAME
+  useEffect(() => {
+    let myToken = localStorage.getItem("My Token");
+    let myCompleteToken = "Bearer " + myToken;
+    fetch(`http://localhost:8080/front/get-username/${id}`, {
+      method: "GET",
+      headers: {
+        Authorization: myCompleteToken,
+      },
+    })
+      .then((data) => data.text())
+      .then((data) => setUsername(data));
+  }, [id]);
 
-//GET USERNAME
-useEffect(() => {
-  let myToken = localStorage.getItem("My Token");
-  let myCompleteToken = "Bearer "+ myToken
-  fetch(`http://localhost:8080/front/get-username/${id}`, {
-    method: "GET",
-    headers: {
- 'Authorization': myCompleteToken,
-},
-})
-.then((data) => data.text())
-.then((data) => setUsername(data))
-}, [id]); 
+  //GET ROLE
+  useEffect(() => {
+    let myToken = localStorage.getItem("My Token");
+    let myCompleteToken = "Bearer " + myToken;
+    fetch(`http://localhost:8080/front/get-user-role/${id}`, {
+      method: "GET",
+      headers: {
+        Authorization: myCompleteToken,
+      },
+    })
+      .then((data) => data.text())
+      .then((data) => setRole(data));
+  }, [id]);
 
-//GET ROLE
- useEffect(() => {
-  let myToken = localStorage.getItem("My Token");
-  let myCompleteToken = "Bearer "+ myToken
-  fetch(`http://localhost:8080/front/get-user-role/${id}`, {
-    method: "GET",
-    headers: {
- 'Authorization': myCompleteToken,
-},
-})
-.then((data) => data.text())
-.then((data) => setRole(data))
-}, [id]); 
+  //SET LOCAL STORAGE DATAS
 
-//SET LOCAL STORAGE DATAS
+  useEffect(() => {
+    localStorage.setItem("role", role);
+    localStorage.setItem("username", username);
+  }, [role, username]);
 
-useEffect(() => {
-  localStorage.setItem('role', role)
-  localStorage.setItem('username', username)
-
-}, [ role, username])
-
-
-
-
-/*   function inputHandle(event) {
+  /*   function inputHandle(event) {
     setSong(event.target.value);
   }
 
@@ -87,26 +83,62 @@ useEffect(() => {
     );
   } */
 
+  //GET RANDOM ARTIST
+  useEffect(() => {
+    let myToken = localStorage.getItem("My Token");
+    let myCompleteToken = "Bearer " + myToken;
+    fetch(`http://localhost:8080/front/getSongsRandomByArtist`, {
+      method: "GET",
+      headers: {
+        Authorization: myCompleteToken,
+      },
+    })
+      .then((data) => data.text())
+      .then((data) => setArtistData([...artistData, data]));
+    // eslint-disable-next-line
+  }, [id]);
+
   return (
     <div className="h-screen w-screen bg-red-800">
       <Navbar />
-      <div className="secondContainer bg-gradient-to-b from-blue-900 to-blue-400 flex flex-col justify-center items-center">
-        <div className="flex justify-center items-around gap-8 h-1/4">
-          <div>asd</div>
-          <div>asd</div>
-          <div>asd</div>
+      <div className="secondContainer bg-gradient-to-b from-blue-900 to-blue-400 flex flex-col justify-center items-center gap-6">
+        <div className="flex justify-around items-center gap-8 h-1/6 w-4/6 border-2 border-violet-900 relative">
+          <div className="flex justify-center items-center">
+            {artistData[1]}
+          </div>
+          <div className="flex justify-center items-center">
+            {artistData[2]}
+          </div>
+          <div className="flex justify-center items-center">
+            {artistData[3]}
+          </div>
+          <p className="absolute top-2 left-5">{artistData[0]}</p>
         </div>
-        <div className="flex justify-center items-around gap-8 h-1/4">
-          <div>asd</div>
-          <div>asd</div>
-          <div>asd</div>
+        <div className="flex justify-around items-center gap-8 h-1/6 w-4/6 border-2 border-violet-900 relative">
+          <div className="flex justify-center items-center">
+            {artistData[1]}
+          </div>
+          <div className="flex justify-center items-center">
+            {artistData[2]}
+          </div>
+          <div className="flex justify-center items-center">
+            {artistData[3]}
+          </div>
+          <p className="absolute top-2 left-5">{artistData[0]}</p>
         </div>
-        <div className="flex justify-center items-around gap-8 h-1/4">
-          <div>asd</div>
-          <div>asd</div>
-          <div>asd</div>
+        <div className="flex justify-around items-center gap-8 h-1/6 w-4/6 border-2 border-violet-900 relative">
+          <div className="flex justify-center items-center">
+            {artistData[1]}
+          </div>
+          <div className="flex justify-center items-center">
+            {artistData[2]}
+          </div>
+          <div className="flex justify-center items-center">
+            {artistData[3]}
+          </div>
+          <p className="absolute top-2 left-5">{artistData[0]}</p>
         </div>
-       </div>
+      </div>
       <Footer />
     </div>
   );
